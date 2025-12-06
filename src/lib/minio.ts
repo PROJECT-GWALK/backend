aeef97fed248521776d841ad9e701232357cccc6
Autsada Wiriya <autsadawiriya@gmail.com>
2025-12-06 23:53:43 +0700

    fix(minio): enable SSL configuration for Minio client

--- src/lib/minio.ts
+++ src/lib/minio.ts
@@ -3,9 +3,12 @@ import * as Minio from "minio";
 const minio = new Minio.Client({
   endPoint: process.env.OBJ_STORAGE_ADDR as string,
   port: Number(process.env.OBJ_STORAGE_PORT),
-  useSSL: false,
+  useSSL: process.env.OBJ_STORAGE_SSL === "true",
   accessKey: process.env.OBJ_ACCESS_KEY as string,
   secretKey: process.env.OBJ_SECRET_KEY as string,
+
+  region: "us-east-1",
+  pathStyle: true,
 });
 
 export function getMinio() {
